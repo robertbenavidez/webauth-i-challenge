4747faececb687d245a93af166cc469d3f92e87c
Robert Benavidez <murabitbenavidez@gmail.com>
2019-09-18 10:52:17 -0400

    cookies config using express-session

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const session = require('express-session');
 
 const UsersRouter = require('./users/users-router.js')
 
@@ -7,16 +8,24 @@ const server = express();
 const cors = require('cors');
 
 
-
-// const db = require('./database/dbConfig.js');
-// const Users = require('./users/users-model.js');
-// const restricted = require('./auth/restricted-middleware.js');
+const sessionConfig = {
+    name: 'oatmeal',
+    secret: process.env.SESSION_SECRET || 'shhhh',
+    cookie: {
+        maxAge: 100 * 60 * 60,
+        secure: false,
+        httpONly: true
+    },
+    resave: false,
+    saveUninitialized: true,
+}
 
 
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+server.use(session(sessionConfig))
 
 server.use('/api/auth', UsersRouter);
 
